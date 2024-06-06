9c2cb89c61193f3f46ac6f942725a292d2d17d4b
ab1123 <asarma626@gmail.com>
2024-06-06 10:49:29 +0530

    sdgds

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,18 @@ import Signup from "./Signup.jsx";
 import Login from "./Login.jsx";
 import Courses from "./Courses.jsx";
 import Home from "./Home.jsx";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router ,Route, Routes } from "react-router-dom";
 
 function App() {
   return (
+    <>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route path="/courses" element={<Courses />} />
     </Routes>
+    </>
   );
 }
 
